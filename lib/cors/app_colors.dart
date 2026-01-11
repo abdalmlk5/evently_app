f41edf401ee7aeb95d4d3a7a0567d00776508b95
Abdelmalek <m7244985@gmail.com>
2026-01-11 07:02:30 +0200

    + init the project

---
+++ lib/cors/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColorsLight {
+  static const Color mainColor = Color(0xFF0E3A99);
+  static const Color disableColor = Color(0xFFB9B9B9);
+  static const Color mainTextColor = Color(0xFF1C1C1C);
+  static const Color secondaryTextColor = Color(0xFF686868);
+  static const Color backgroundColor = Color(0xFFF4F7FF);
+  static const Color inputsColor = Color(0xFFFFFFFF);
+  static const Color strokeColor = Color(0xFFF0F0F0);
+  static const Color redColor = Color(0xFFFF3232);
+}
+
+class AppColorsDark {
+  static const Color mainColor = Color(0xFF457AED);
+  static const Color disableColor = Color(0xFFB9B9B9);
+  static const Color mainTextColor = Color(0xFFFFFFFF);
+  static const Color secondaryTextColor = Color(0xFFD6D6D6);
+  static const Color backgroundColor = Color(0xFF000F30);
+  static const Color inputsColor = Color(0xFF001440);
+  static const Color strokeColor = Color(0xFF002D8F);
+  static const Color redColor = Color(0xFFFF3232);
+}
